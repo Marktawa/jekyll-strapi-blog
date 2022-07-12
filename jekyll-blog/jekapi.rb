a203e9e6e6ceea063cd7f16d77a39eecdca435f2
McTawa <markmunyaka@gmail.com>
2022-07-12 22:23:38 +0000

    Removed comments

--- jekyll-blog/jekapi.rb
+++ jekyll-blog/jekapi.rb
@@ -6,25 +6,6 @@ response = File.read('db.json')
 
 result = JSON.parse(response, object_class: OpenStruct)
 
-=begin
-post_file_name_suffix = result.data[0].attributes.slug
-post_file_name_prefix = result.data[0].attributes.createdAt
-post_file_name_prefix.slice!(10..24)
-post_file_name = "#{post_file_name_prefix}-#{post_file_name_suffix}"
-
-post_file_title = result.data[0].attributes.title
-
-post_file_date = result.data[0].attributes.createdAt
-
-post_file_category = result.data[0].attributes.category.data.attributes.name
-
-post_file_description = result.data[0].attributes.description
-
-post_file_author = result.data[0].attributes.author.data.attributes.name
-
-post_file_content = result.data[0].attributes.content
-=end
-
 result.data.each do |document|
     post_file_name_suffix = document.attributes.slug
     post_file_name_prefix = document.attributes.createdAt

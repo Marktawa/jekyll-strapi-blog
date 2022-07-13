69e6c95d7dfcb095d692a822ffef23be90ea9148
McTawa <markmunyaka@gmail.com>
2022-07-13 01:10:34 +0000

    Simple script

--- jekyll-blog/jekapi.rb
+++ jekyll-blog/jekapi.rb
@@ -2,12 +2,15 @@ require 'net/http'
 require 'json'
 require 'ostruct'
 
-response = File.read('db.json')
+#response = File.read('../tests/db.json')
+url = 'http://localhost:1337/api/posts?populate=*'
+uri = URI(url)
+response = Net::HTTP.get(uri)
 
 result = JSON.parse(response, object_class: OpenStruct)
 
 result.data.each do |document|
-    post_file_name_suffix = document.attributes.slug
+    post_file_name_suffix = document.attributes.Slug
     post_file_name_prefix = document.attributes.createdAt
     post_file_name_prefix.slice!(10..24)
     post_file_name = "#{post_file_name_prefix}-#{post_file_name_suffix}"
@@ -16,11 +19,9 @@ result.data.each do |document|
 
     post_file_date = document.attributes.createdAt
 
-    post_file_category = document.attributes.category.data.attributes.name
-
     post_file_description = document.attributes.description
 
-    post_file_author = document.attributes.author.data.attributes.name
+    post_file_author = document.attributes.author.data.attributes.username
 
     post_file_content = document.attributes.content
 
@@ -29,12 +30,10 @@ result.data.each do |document|
     post_file.puts("layout: post")
     post_file.puts("title: \"#{post_file_title}\"")
     post_file.puts("date: #{post_file_date}")
-    post_file.puts("category: #{post_file_category}")
     post_file.puts("description: #{post_file_description}")
     post_file.puts("author: #{post_file_author}")
     post_file.puts("---")
     post_file.puts("#{post_file_content}")
   end
-
 
 

ba635a8641a9c41917c2ff919a83aaaf13e898d2
McTawa <markmunyaka@gmail.com>
2022-07-15 13:16:10 +0000

    Added cloudinary provider plugin

---
+++ jekyll-blog-backend/config/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+    // ...
+    seo: {
+      enabled: true,
+    },
+    // ...
+    upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
+      // ...
+  });

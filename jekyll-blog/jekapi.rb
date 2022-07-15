f0aa39228e1cff4e971d6eb5542dda3130d625cf
McTawa <markmunyaka@gmail.com>
2022-07-15 07:46:38 +0000

    Cleaned up script

--- jekyll-blog/jekapi.rb
+++ jekyll-blog/jekapi.rb
@@ -1,7 +1,9 @@
+#jekapi.rb
+
 require 'net/http'
 require 'json'
 require 'ostruct'
-#response = File.read('../tests/db.json')
+
 url = 'http://localhost:1337/api/posts?populate=*'
 uri = URI(url)
 response = Net::HTTP.get(uri)
@@ -9,11 +11,11 @@ result = JSON.parse(response, object_class: OpenStruct)
 result.data.each do |document|
     post_file_locale_def = document.attributes.locale
     post_file_name_suffix = document.attributes.Slug
+    post_file_date = document.attributes.createdAt
     post_file_name_prefix = document.attributes.createdAt
     post_file_name_prefix.slice!(10..24)
     post_file_name = "#{post_file_name_prefix}-#{post_file_name_suffix}"
     post_file_title = document.attributes.title
-    post_file_date = document.attributes.createdAt
     post_file_description = document.attributes.description
     post_file_author = document.attributes.author.data.attributes.username
     post_file_content = document.attributes.content
@@ -30,7 +32,6 @@ result.data.each do |document|
     post_file.puts("#{post_file_content}")
 
     post_file_locale = document.attributes.localizations.data[0].attributes.locale
-    #post_file_name_locale = "#{post_file_name_prefix}-#{post_file_name_suffix}-#{post_file_locale}"
     post_file_title_locale = document.attributes.localizations.data[0].attributes.title
     post_file_description_locale = document.attributes.localizations.data[0].attributes.description
     post_file_content_locale = document.attributes.localizations.data[0].attributes.content
